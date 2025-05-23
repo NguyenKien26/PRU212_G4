45323150b3f420ba3c68057925c320e1ba22a76f
Nguyen Tran Khanh Kien <nguyenkien264038@gmail.com>
2025-05-23 21:52:12 +0700

    push project

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject instructionsPanel; // Gán từ Inspector
+    public GameObject instructionButton;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+
+    public void ShowInstructions()
+    {
+        instructionsPanel.SetActive(true);
+        instructionButton.SetActive(false); 
+    }
+
+    public void HideInstructions()
+    {
+        instructionsPanel.SetActive(false);
+        instructionButton.SetActive(true); 
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
